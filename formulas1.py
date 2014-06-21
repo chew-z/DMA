a4b34087195afb860525a1e3cb06a7ffc1db8b79
chew-z <chew-z@outlook.com>
2014-06-21 08:40:06 +0800

    Udoskonaliłem i dodałem wizalizację dla badania Profits & Loss results

--- formulas1.py
+++ formulas1.py
@@ -5,7 +5,15 @@ Created on Fri Jun 20 21:06:34 2014
 @author: chew-z
 """
 import numpy as np
-import scipy.io as scio
+#import scipy.io as scio
+import matplotlib.pyplot
+
+def autocorr(x):
+    result = np.correlate(x, x, mode='full')
+    maxcorr = np.argmax(result)
+    #print 'maximum = ', result[maxcorr]
+    result = result / result[maxcorr]
+    return result[result.size/2:]
 
 def HPR(X, f):
 # Holding Period Return - dla wektora X, danego f i największej straty min(X)
@@ -16,17 +24,47 @@ def TWR(X, f):
     return np.prod(HPR(X, f))
 
 def f(X):
-    f = 0.01
+    f = 0.00
     k = 0.01
     while ( TWR(X, f) < TWR(X, f + k) ):
         if f > 1.0:
             break
         f = f + k
+#if f -- 0 or f > 1 raise exception
     return(f)
+    
+def gvsf(X): # G vs. f (or GAT vs f or TWR vs f)
+    n = len(X)
+    # m = min(X)
+    Y = []
+    for i in xrange(100):
+        fi = float(0.01*i)
+        G = TWR(PL,fi) ** (1.0/n) - 1 # < 0 oznacza straty
+        #GAT = G * (- m/fi);
+        Y = Y + [[fi, G]]
+    return(np.array(Y))
 
 #d = scio.loadmat("PL.mat") #Matlab matrix with H1Close & DMA200
 #PL = np.array(d['PL'][:,0]) #
-PL = 100*np.random.rand(10)-40
+
+#Using true random vector as P&L you can sometimes get incalculable results (exceptions etc.) try another
+PL = 100*np.random.rand(10)-40 #skewed into profits a little for more practical demonstration
+
+#1 Check P&L autocorrelation
+matplotlib.pyplot.subplot(221)
+matplotlib.pyplot.plot(autocorr(PL))
+#2 Plot histogram P&L
+matplotlib.pyplot.subplot(222)
+matplotlib.pyplot.hist(PL)
+#3 Plot cumulation of P&L
+matplotlib.pyplot.subplot(223)
+matplotlib.pyplot.plot(np.cumsum(PL))
+#4 Plot G vs. f - if < 1 you are waisting time
+X = gvsf(PL)
+matplotlib.pyplot.subplot(224)
+matplotlib.pyplot.plot(X[:,0], X[:,1]) 
+
+matplotlib.pyplot.show()
 
 f = f(PL)
 
